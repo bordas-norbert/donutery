8b052bd684512ac6b58575d3dd31ba9b823279bb
bordas-norbert <b.norbert101@gmail.com>
2022-12-22 23:55:06 +0200

    add project
    Add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupComponent,
+    HomeComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
