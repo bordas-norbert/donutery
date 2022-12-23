19ca8bcade987b8fe93a955d724731cfdd6a52b5
bordas-norbert <b.norbert101@gmail.com>
2022-12-23 12:27:09 +0200

    Add dynamic cards

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
-
+  cardItems = [
+    { "id" : 1, "title": 'Chocolate glazed donut', "description": "If you are craving for something tasty...", "toolTip":""},
+    { "id" : 2, "title": 'Salted caramel glaze...', "description": "Salty on top, sweet in the inside...", "toolTip":"Salty caramel glazed donut"},
+    { "id" : 3, "title": 'Frosty donut', "description": "Mint sugar topping with blueberry filling.", "toolTip":""},
+  ]
 }
